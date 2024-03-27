adbabb400e245c6ce392ea05efd945ade4901151
Rahul <bhaleraor17@gmail.com>
2024-03-27 18:30:04 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isSubmitted, setIsSubmitted] = useState(true);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
@@ -15,39 +15,51 @@ function App() {
     }
 
     if (username === "user" && password === "password") {
-
       setError("");
-      setIsSubmitted(false);
-
-    }
-    else {
-      setError("Invalid username or password");
       setIsSubmitted(true);
+    } else {
+      setError("Invalid username or password");
+      setIsSubmitted(false);
     }
-  }
+  };
+
   return (
-    <div >
+    <div>
       <h1>Login Page</h1>
 
-      {!isSubmitted ? (<p>Welcome {username}!</p>)
-        : (<form onSubmit={handleSubmit}>
+      {!isSubmitted ? (
+        <form onSubmit={handleSubmit}>
           {error && <p>{error}</p>}
           <div>
-            <label >
+            <label>
               Username:
-              <input type="text" placeholder="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+              <input
+                type="text"
+                placeholder="username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </label>
           </div>
           <div>
             <label>
               Password:
-              <input type="password" placeholder="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </label>
           </div>
           <button type="submit">Submit</button>
-        </form>)}
-
-    </div >
+        </form>
+      ) : (
+        <p>Welcome, {username}!</p>
+      )}
+    </div>
   );
 }
 
